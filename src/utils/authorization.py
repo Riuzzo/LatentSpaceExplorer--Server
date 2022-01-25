52dfe7b999addb8d9fb634ffd993dc4fbccb49c4
Lucas Puerari <lucas.puerari@unimib.it>
2022-01-25 15:53:25 +0100

    Handle ConnectionError with retries

--- src/utils/authorization.py
+++ src/utils/authorization.py
@@ -1,4 +1,3 @@
-import time
 from fastapi import Header, Request
 from pydantic import Required
 
@@ -11,17 +10,10 @@ class AuthError(Exception):
 
 
 def authorization(request: Request, user_id: str = Header(Required)):
-    attempts = 3
-
     storage = request.state.storage
 
     user_dir = '{}{}'.format(constants.NEXTCLOUD_PREFIX_USER_DIR, user_id)
     user_dir_exist = storage.dir_exist(user_dir)
-
-    while not user_dir_exist and attempts > 0:
-        user_dir_exist = storage.dir_exist(user_dir)
-        attempts -= 1
-        time.sleep(3)
 
     if not user_dir_exist:
         raise AuthError(user_id=user_dir)
