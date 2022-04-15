3e93a097187e06f9c25ba85753e75d04d31a37f8
Thomas Cecconello <thomas.cecconello@unimib.it>
2022-04-15 17:50:41 +0200

    Added paths as comment

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,3 +1,37 @@
+### Paths
+
+# "lse-${user_id}/${experiment_id}"
+# "lse-${user_id}/${experiment_id}/metadata.json"
+# "lse-${user_id}/${experiment_id}/embeddings.json"
+# "lse-${user_id}/${experiment_id}/labels.json"
+
+# "lse-${user_id}/${experiment_id}/reductions"
+# "lse-${user_id}/${experiment_id}/reductions/${reduction_id}/metadata.json"
+# "lse-${user_id}/${experiment_id}/reductions/${reduction_id}/reduction.json"
+
+# "lse-${user_id}/${experiment_id}/clusters"
+# "lse-${user_id}/${experiment_id}/clusters/${cluster_id}/metadata.json"
+# "lse-${user_id}/${experiment_id}/clusters/${cluster_id}/cluster.json"
+# "lse-${user_id}/${experiment_id}/clusters/${cluster_id}/score.json"
+# "lse-${user_id}/${experiment_id}/clusters/${cluster_id}/silhouette.json"
+
+# "lse-demo/${experiment_id}"
+# "lse-demo/${experiment_id}/data-${user_id}"
+# "lse-${user_id}/${experiment_id}/metadata.json"
+# "lse-${user_id}/${experiment_id}/embeddings.json"
+# "lse-${user_id}/${experiment_id}/labels.json"
+
+# "lse-demo/${experiment_id}/data-${user_id}"/reductions"
+# "lse-demo/${experiment_id}/data-${user_id}"/reductions/${reduction_id}/metadata.json"
+# "lse-demo/${experiment_id}/data-${user_id}"/reductions/${reduction_id}/reduction.json"
+
+# "lse-demo/${experiment_id}/data-${user_id}"/clusters"
+# "lse-demo/${experiment_id}/data-${user_id}"/clusters/${cluster_id}/metadata.json"
+# "lse-demo/${experiment_id}/data-${user_id}"/clusters/${cluster_id}/cluster.json"
+# "lse-demo/${experiment_id}/data-${user_id}"/clusters/${cluster_id}/score.json"
+# "lse-demo/${experiment_id}/data-${user_id}"/clusters/${cluster_id}/silhouette.json"
+
+
 NEXTCLOUD_PREFIX_USER_DIR = 'lse-'
 DEMO_DIR = 'lse-demo'
 
