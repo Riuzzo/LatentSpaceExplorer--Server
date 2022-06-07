01a3b54ed5b596b2dbd3471f849773c07fced3ec
Thomas Cecconello <thomas.cecconello@unimib.it>
2022-06-07 16:11:24 +0200

    Removed Authorization check in order to show demos

--- src/utils/authorization.py
+++ src/utils/authorization.py
@@ -14,8 +14,9 @@ class AuthError(Exception):
 
 
 def authorization(request: Request, user_id: str = Header(Required)):
+    """
     storage = request.state.storage
-
+    
     user_dir = '{}{}'.format(constants.NEXTCLOUD_PREFIX_USER_DIR, user_id)
     initial_time = time.time()
     user_dir_exist = storage.dir_exist(user_dir)
@@ -25,4 +26,5 @@ def authorization(request: Request, user_id: str = Header(Required)):
         raise AuthError(user_id=user_dir)
 
     logger.debug(message='User dir exist', duration=elapsed, action='authorization', subaction="check_folder_exist", status='SUCCED', resource='lse-service', userid=user_id)
+    """
     return user_id
